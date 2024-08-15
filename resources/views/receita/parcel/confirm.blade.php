954360ed83aa925488affc85f670537a6d9e41bb
Everton da Rosa <everton3x@gmail.com>
2024-08-15 15:42:15 -0300

    fix: Update confirm.blade.php

--- resources/views/receita/parcel/confirm.blade.php
+++ resources/views/receita/parcel/confirm.blade.php
@@ -1,4 +1,4 @@
-<x-app-layout>
+a<x-app-layout>
 
     <x-slot name="title">
         Confirmar parcelamento da receita
@@ -18,7 +18,7 @@
                         label="Período" name="periodo[{{ $receita['parcela'] }}]" id="periodo[{{ $receita['parcela'] }}]" />
                     <x-input.parcelas value="{{ old('parcela', $receita['parcela']) }}" required label="Parcela" min="1" name="parcela[{{ $receita['parcela'] }}]" id="parcela[{{ $receita['parcela'] }}]" />
                     <x-input.descricao value="{{ old('descricao', $receita['descricao']) }}" required name="descricao[{{ $receita['parcela'] }}]" id="descricao[{{ $receita['parcela'] }}]" />
-                    <x-input.devedor value="{{ old('devedor', $receita['devedor']) }}" :devedores="pessoas()" required name="devedor[{{ $receita['parcela'] }}]" id="devedor[{{ $receita['parcela'] }}]" />
+                    <x-input.pessoa value="{{ old('devedor', $receita['devedor']) }}" :devedores="pessoas()" required name="devedor[{{ $receita['parcela'] }}]" id="devedor[{{ $receita['parcela'] }}]" />
                     <x-input.valor value="{{ old('valor', $receita['valor']) }}" required min="0.01" name="valor[{{ $receita['parcela'] }}]" id="valor[{{ $receita['parcela'] }}]" />
                     <x-input.vencimento value="{{ old('vencimento', $receita['vencimento']) }}" label="Vencimento" name="vencimento[{{ $receita['parcela'] }}]" id="vencimento[{{ $receita['parcela'] }}]" />
                     <x-input.agrupador value="{{ old('agrupador', $receita['agrupador']) }}" name="agrupador[{{ $receita['parcela'] }}]" id="agrupador[{{ $receita['parcela'] }}]" />
